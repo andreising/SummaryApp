86055e61faa0183260eeb3173e25b37e02ba7551
Андрей Сингелейцев <andreysamarachess@mail.ru>
2025-05-28 16:59:15 +0400

    FinishedFragment.kt renamed to FinishGameFragment.kt added ui binding in fragment

--- app/src/main/java/com/andreising/summaryapp/presentation/fragments/finished/FinishGameFragment.kt
+++ app/src/main/java/com/andreising/summaryapp/presentation/fragments/finished/FinishGameFragment.kt
@@ -5,34 +5,45 @@ import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.andreising.summaryapp.R
-import com.andreising.summaryapp.databinding.FragmentFinishedBinding
+import com.andreising.summaryapp.databinding.FragmentFinishGameBinding
 import com.andreising.summaryapp.domain.models.GameResult
 import com.andreising.summaryapp.presentation.fragments.game.GameFragment
 import com.andreising.summaryapp.presentation.navigation.popBackStack
 import com.andreising.summaryapp.presentation.navigation.requireParcelable
 import dev.androidbroadcast.vbpd.viewBinding
 
-class FinishedFragment : Fragment(R.layout.fragment_finished) {
+class FinishGameFragment : Fragment(R.layout.fragment_finish_game) {
 
-    val binding: FragmentFinishedBinding by viewBinding(FragmentFinishedBinding::bind)
+    val binding: FragmentFinishGameBinding by viewBinding(FragmentFinishGameBinding::bind)
 
     private lateinit var gameResult: GameResult
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        gameResult = requireArguments().requireParcelable<GameResult>(GAME_RESULT_KEY)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        gameResult = requireArguments().requireParcelable<GameResult>(GAME_RESULT_KEY)
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 retryGame()
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
-        binding.retryButton.setOnClickListener { retryGame() }
+        bindResult(gameResult)
     }
+
+    private fun bindResult(result: GameResult) = with(binding) {
+        resultImage.setImageResource(
+            if (result.correctAnswers >= result.requiredCorrectAnswers)
+                R.drawable.ic_smile
+            else
+                R.drawable.ic_sad
+        )
+
+        rightAnswersText.text = getString(R.string.right_answers, result.correctAnswers.toString())
+        requiredAnswersText.text =
+            getString(R.string.required_answers, result.requiredCorrectAnswers.toString())
+        timeSpentText.text = getString(R.string.time_spent, result.totalTimeSec.toString())
+    }
+
 
     private fun retryGame() {
         requireActivity().popBackStack(GameFragment.NAME, true)
@@ -42,8 +53,8 @@ class FinishedFragment : Fragment(R.layout.fragment_finished) {
 
         private const val GAME_RESULT_KEY = "game_result_key"
 
-        fun newInstance(gameResult: GameResult): FinishedFragment {
-            return FinishedFragment().apply {
+        fun newInstance(gameResult: GameResult): FinishGameFragment {
+            return FinishGameFragment().apply {
                 arguments = Bundle().apply {
                     putParcelable(GAME_RESULT_KEY, gameResult)
                 }
